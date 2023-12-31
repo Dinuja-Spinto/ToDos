f8821e9778ce8290267464a5fdbca3aebe003548
Dinuja Pinto <sri.prabasha@gmail.com>
2024-01-01 01:27:25 +0530

    55 subscriber

--- backend/todos/src/main/java/com/todos/restfulwebservice/HelloWorldController.java
+++ backend/todos/src/main/java/com/todos/restfulwebservice/HelloWorldController.java
@@ -2,6 +2,7 @@ package com.todos.restfulwebservice;
 
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins ="http://localhost:4200/")
 @RestController
 public class HelloWorldController {
     @RequestMapping(method = RequestMethod.GET,path="/hello-world")
--- todo/src/app/welcome/welcome.component.ts
+++ todo/src/app/welcome/welcome.component.ts
@@ -21,7 +21,7 @@ export class WelcomeComponent {
 
   getWelcomeMsg(){
     //this.service.executeHelloWorldBeanService();
-    console.log(this.service.executeHelloWorldBeanService());
+    console.log(this.service.executeHelloWorldBeanService().subscribe());
   }
 
 }
