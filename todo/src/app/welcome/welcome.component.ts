5ce25690cb356e65f3708a764a57a613641ba471
Dinuja Pinto <sri.prabasha@gmail.com>
2024-01-01 00:15:31 +0530

    first git commit of 2024

--- todo/src/app/welcome/welcome.component.ts
+++ todo/src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -10,11 +11,17 @@ export class WelcomeComponent {
 
   name =''
 
-  constructor(private route : ActivatedRoute){}
+  constructor(private route : ActivatedRoute,
+    private service:WelcomeDataService){}
 
   ngOnInit(){
     this.name = this.route.snapshot.params['name']
     console.log(this.route.snapshot.params['name'])
+  }
+
+  getWelcomeMsg(){
+    this.service.executeHelloWorldBeanService();
+    //console.log("get welcome message");
   }
 
 }
