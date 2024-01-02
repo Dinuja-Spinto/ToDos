963fa4e6776fc3bb635f7c30452256a60cff6762
Dinuja Pinto <sri.prabasha@gmail.com>
2024-01-03 00:08:09 +0530

    61

--- todo/src/app/list-to-dos/list-to-dos.component.ts
+++ todo/src/app/list-to-dos/list-to-dos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoDataService } from '../service/data/todo-data.service';
 
 export class Todo{
   constructor(
@@ -15,10 +16,23 @@ export class Todo{
   styleUrls: ['./list-to-dos.component.css']
 })
 export class ListToDosComponent {
+todos:Todo[] | undefined;
+  // todos=[
+  //   new Todo(1, 'Learn to Code', false, new Date()),
+  //   new Todo(2, 'Become an expert', false, new Date()),
+  //   new Todo(3, 'visit sri lanka', false, new Date())
+  // ]
 
-  todos=[
-    new Todo(1, 'Learn to Code', false, new Date()),
-    new Todo(2, 'Become an expert', false, new Date()),
-    new Todo(3, 'visit sri lanka', false, new Date())
-  ]
+  constructor(
+    private todoService : TodoDataService
+  ){}
+  ngOnInit(){
+      this.todoService.retriveAllTodos("dinuja").subscribe(
+        response =>{
+          console.log(response);
+          this.todos = response;
+        }
+      );
+  }
+
 }
---
+++ todo/src/app/service/data/todo-data.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Todo } from 'src/app/list-to-dos/list-to-dos.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoDataService {
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  retriveAllTodos(username:string){
+    return this.http.get<Todo[]>(`http://localhost:8081/users/${username}/todos`)
+  }
+}
