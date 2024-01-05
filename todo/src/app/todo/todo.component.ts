e77a9d7593a69da9ea4dc19f5ef982041cea99f7
Dinuja Pinto <sri.prabasha@gmail.com>
2024-01-06 02:59:36 +0530

    68

--- todo/src/app/todo/todo.component.ts
+++ todo/src/app/todo/todo.component.ts
@@ -12,14 +12,15 @@ export class TodoComponent {
 
   id:number | undefined;
   todo:Todo | undefined;
-  
+
   constructor(
     private todoService: TodoDataService,
     private route: ActivatedRoute
   ){}
 
   ngOnInit(){
-    this.id = this.route.snapshot.params['id']
+    this.id = this.route.snapshot.params['id'];
+    this.todo = new Todo(1,'',false,new Date());
     this.todoService.retreveTodo('Dinuja',this.id).subscribe(
       data=> this.todo = data
     )
