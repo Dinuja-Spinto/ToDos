22ff8f717b193f4bc61665b9def52f40cffa790d
Dinuja Pinto <sri.prabasha@gmail.com>
2024-01-11 23:54:26 +0530

    81

--- backend/todos/src/main/java/com/todos/restfulwebservice/basic/auth/SpringSecurityConfigBasicAuth.java
+++ backend/todos/src/main/java/com/todos/restfulwebservice/basic/auth/SpringSecurityConfigBasicAuth.java
@@ -1,2 +1,15 @@
-package com.todos.restfulwebservice.basic.auth;public class SpringSecurityConfigBasicAuth {
+package com.todos.restfulwebservice.basic.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfigBasicAuth extends WsConfigurerAdapter {
+
+    protected void configure(HttpSecurity http) throws Exception{
+      //  http.csrf().disable().antMatchers(HttpMethod.OPTIONS,"/**").any;
+    }
 }
